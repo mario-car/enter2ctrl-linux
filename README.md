d8a3295ba56f21f3fc3675074a6b347e6840a757
carma <carma@lokomotiva.com>
2020-03-06 20:16:46 +0000

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # keyboard-linux
 CapsLock as Control when pressed and Escape when released; Enter as Control when pressed
+
+This small script uses xmodmap and xcape to modify keyboard to have Control modifier easily accessable
+with pinkies on both sides of keyboard.
+Make sure you run it at startup.
---
+++ fixKeyboard.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Swap Caps_Lock and Control_L
+xmodmap -e "clear lock"
+xmodmap -e "remove Control = Control_L"
+xmodmap -e "keycode 66 = Control_L"
+xmodmap -e "keycode 135 = Super_R"
+xmodmap -e "add Control = Control_L"
+
+xcape -e 'Control_L=Escape'
+
+# Map an unused modifier's keysym to the spacebar's keycode and make it a
+# control modifier. It needs to be an existing key so that emacs won't
+# spazz out when you press it. Hyper_L is a good candidate.
+spare_modifier="Hyper_L"
+xmodmap -e "keycode 36 = $spare_modifier"
+xmodmap -e "remove mod4 = $spare_modifier" # hyper_l is mod4 by default
+xmodmap -e "add Control = $spare_modifier"
+
+# Map space to an unused keycode (to keep it around for xcape to
+# use).
+xmodmap -e "keycode any = Return"
+
+# Finally use xcape to cause the space bar to generate a space when tapped.
+xcape -e "$spare_modifier=Return"
